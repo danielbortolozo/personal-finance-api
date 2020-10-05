f7839e11f8ae108088db3f7f47eddc0fcfe3feed
Daniel Rocca Bortolozo <danielbortolozo@hotmail.com>
2020-10-05 00:04:44 -0300

    Secund commit

---
+++ src/main/java/br/com/sisdb/myfinances/MyfinancesApplication.java
@@ -0,0 +1,13 @@
+package br.com.sisdb.myfinances;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyfinancesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyfinancesApplication.class, args);
+	}
+
+}
